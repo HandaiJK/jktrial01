866f39172397abcf59eaa037ee09e19e07e27af1
Maxfield Walker <maxfield.walker@gmail.com>
2018-05-17 17:13:33 +0900

    Add template file

---
+++ lifegame.c
@@ -0,0 +1,11 @@
+int main()
+{
+    int w = 10;
+    int h = 10;
+    int alive_cells[] = {11, 22, 3, 13, 23};
+    int alive_cells_count = 5;
+
+    lifegame(w, h, alive_cells, alive_cells_count);
+
+    return 0;
+}
