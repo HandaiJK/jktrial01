8cd46a24cb90e76d79b7f25cb0873f5e311b5c41
Maxfield Walker <maxfield.walker@gmail.com>
2018-05-26 20:34:45 +0900

    Tweak

--- lifegame.c
+++ lifegame.c
@@ -2,10 +2,10 @@ int main()
 {
     int w = 10;
     int h = 10;
-    int alive_cells[] = {11, 22, 3, 13, 23};
-    int alive_cells_count = 5;
+    int living_cells[] = {11, 22, 3, 13, 23};
+    int living_cells_count = 5;
 
-    lifegame(w, h, alive_cells, alive_cells_count);
+    lifegame(w, h, living_cells, living_cells_count);
 
     return 0;
 }
